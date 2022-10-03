862b1f808ca9a19aae96c0815debf67972a028cb
Arush Shrivastava <66080016+M4dar4@users.noreply.github.com>
2022-10-03 12:28:37 +0530

    Update sc4nit.py

--- sc4nit.py
+++ sc4nit.py
@@ -5,6 +5,7 @@ from package import subdomain
 
 from termcolor import colored
 import os
+import sys
 import pyfiglet
 import random
 
@@ -38,4 +39,8 @@ def choose():
 
 os.system("clear")
 banner()
+help_option = "subdomain finder  -->  Finds subdomain of given domain. It uses a subdomain prefix list located at /usr/local/bin/package/subdomains.txt. You can also append a new prefix to it.\nport scanner  -->  It scans port of given IP address or you can copy paste subdomain name displayed after using subdomain finder."
+if (sys.argv[1] == "-h"):
+	print (colored(help_option, 'blue'))
+
 choose()
